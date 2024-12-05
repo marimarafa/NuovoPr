e7bdb443f74804cdee5b922102c2b9342af17ed9
marimarafa <arafamarim166@gmail.com>
2024-12-05 13:36:16 +0100

    ..

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -10,23 +10,27 @@ function App() {
   return (
     <>
     <Card
+      isVisitato = {true}
       titolo = "Tokyo"
       descrizione = "Hello there"
       image = "https://images.unsplash.com/photo-1732878796195-0479be305faf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNDV8fHxlbnwwfHx8fHw%3D">
     </Card>
     <Card
+    isVisitato = {true}
     titolo = "London"
     descrizione = "Good morning"
     image = "https://images.unsplash.com/photo-1733234976396-87cf34ae6038?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8" 
     ></Card>
     <Card 
-    titolo = "____________________________"
+    isVisitato = {true}
+    titolo = "Heloooooooooo"
     descrizione ="hello"
     image = "https://plus.unsplash.com/premium_photo-1674489157120-9c386f7173d9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2OHx8fGVufDB8fHx8fA%3D%3D" 
     >
     </Card>
     <Card
-    titolo = "!!!!!!!!!!!!!!!!!!!"
+    isVisitato = {false}
+    titolo = "?????????????????????"
     descrizione = "-------------------------"
     image = "https://images.unsplash.com/photo-1733036016861-0541eb76dac5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3MHx8fGVufDB8fHx8fA%3D%3D"
     >
