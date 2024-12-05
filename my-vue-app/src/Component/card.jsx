fcb118b821172142150b8ed8e0e295de2ededf69
marimarafa <arafamarim166@gmail.com>
2024-12-05 13:42:22 +0100

    ..

--- my-vue-app/src/Component/card.jsx
+++ my-vue-app/src/Component/card.jsx
@@ -1,24 +1,8 @@
-  
 function Card(props){
     const titolo = props.titolo
     const descrizione = props.descrizione
     const image = props.image
     const isVisitato = props.isVisitato
-
-    if(isVisitato){
-        return (
-            <div>
-                <img src= {image}>
-                </img>
-                <div>
-                    <h2>{titolo}</h2>
-                    <p> {descrizione} </p>
-                    <span ><b> Visitato </b></span>
-                </div>
-            </div>
-
-        )
-} else {
     return (
         <div>
             <img src= {image}>
@@ -26,12 +10,10 @@ function Card(props){
             <div>
                 <h2>{titolo}</h2>
                 <p> {descrizione} </p>
-                <span ><b> NON Visitato </b></span>
-
+                {isVisitato && <span ><b>  Visitato  </b></span>}
             </div>
         </div>
 
     )
-}
 }
 export default Card;
